ff9e878c9391467e7204eb8f9c93776690ea817f
liam <liamkeany72@gmail.com>
2026-02-07 10:24:55 +0000

    Added Diaplay menue function

--- AlgorithmTimer/AlgorithmUI.cs
+++ AlgorithmTimer/AlgorithmUI.cs
@@ -7,16 +7,28 @@ namespace AlgorithmTimer
     internal class AlgorithmUI
     {
         string title {get; set;}
+        public string[] algorithms = ["Bubble Sort", "Selection Sort", "Insertion Sort", "Merge Sort", "Quick Sort", "Heap Sort", "Radix Sort", "Bucket Sort", "Shell Sort", "Counting Sort", "All"]; 
         public AlgorithmUI()
         {
             using StreamReader reader = new StreamReader(@"C:\Algorithm Timer\AlgorithmTimer\Title.txt");
             this.title = reader.ReadToEnd();
         }
 
-        public void Displaymenu()
+        public void Displaymenu(int selected)
         {
             System.Console.WriteLine(title);
-            System.Console.WriteLine("1.) Bubble Sort");
+            for(int i = 0; i < algorithms.Length; i++)
+            {
+                if(i == selected)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    System.Console.Write($"> {algorithms[i]}");
+                    Console.ResetColor();
+                    System.Console.WriteLine();
+                }
+                else{System.Console.WriteLine(algorithms[i]);}
+                
+            }
         }
     }
 }
